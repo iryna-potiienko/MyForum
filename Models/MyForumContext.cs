4f63f02c4f3789cdb9a8c897d605a86143e602f6
Iryna I. Potiienko <iryna.potiienko@gmail.com>
2021-09-04 17:03:09 +0300

    Created models and context (needs to deal with database)

---
+++ Models/MyForumContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyForum.Models
+{
+    public class MyForumContext: DbContext
+    {
+        public virtual DbSet<Chapter> Chapters { get; set; }
+        public virtual DbSet<Subject> Subjects { get; set; }
+        public virtual DbSet<Message> Messages { get; set; }
+        public virtual DbSet<UserProfile> UserProfiles { get; set; }
+        public virtual DbSet<UserRole> UserRoles { get; set; }
+        public virtual DbSet<UserRoleMapping> UserRoleMappings { get; set; }
+
+        public MyForumContext(DbContextOptions<MyForumContext> options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
