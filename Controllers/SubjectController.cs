507019c856fff8b29b801c68205ba8af7adcf6a1
Iryna I. Potiienko <iryna.potiienko@gmail.com>
2021-09-05 23:12:38 +0300

    Created controllers (needs additional abilities)

---
+++ Controllers/SubjectController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyForum.Models;
+
+namespace MyForum.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly MyForumContext _context;
+
+        public SubjectController(MyForumContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Subject
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Subject>>> GetSubjects()
+        {
+            return await _context.Subjects
+                //.Include(ch=>ch.Chapter)
+                .ToListAsync();
+        }
+
+        // GET: api/Subject/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subject>> GetSubject(int id)
+        {
+            var subject = await _context.Subjects.FindAsync(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return subject;
+        }
+
+        // PUT: api/Subject/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubject(int id, Subject subject)
+        {
+            if (id != subject.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subject
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Subject>> PostSubject(Subject subject)
+        {
+            _context.Subjects.Add(subject);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubject", new { id = subject.Id }, subject);
+        }
+
+        // DELETE: api/Subject/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubject(int id)
+        {
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subjects.Remove(subject);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SubjectExists(int id)
+        {
+            return _context.Subjects.Any(e => e.Id == id);
+        }
+    }
+}
